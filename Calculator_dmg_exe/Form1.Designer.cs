8b82c4c21696d1e728d8d58d8aacf2502e1b4370
Yousef10 <yousef_espetan@yahoo.com>
2022-11-02 18:12:33 +0300

    Main Form Size

--- Calculator_dmg_exe/Form1.Designer.cs
+++ Calculator_dmg_exe/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 366);
+            this.ClientSize = new System.Drawing.Size(280, 400);
             this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
