01ade35f5803851ae8bca9cbae9bd7ba79e1d003
Yousef10 <yousef_espetan@yahoo.com>
2022-11-02 18:24:36 +0300

    Button Size Misc Options

--- Calculator_dmg_exe/iosButtons.cs
+++ Calculator_dmg_exe/iosButtons.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,76 @@ namespace Calculator_dmg_exe
 {
     public partial class iosButtons : UserControl
     {
+
+        public enum CollapseDirection
+        {
+            Normal,
+            Big,
+            Wide
+        }
+        #region Collapse Direction
+
+        [Browsable(true), DefaultValue("Normal"), Description("Buttons's size")]
+        [ListBindable(true), Editor(typeof(ComboBox), typeof(UITypeEditor))]
+        public CollapseDirection _direction = CollapseDirection.Normal;
+        public CollapseDirection Direction
+        {
+            get { return _direction; }
+            set
+            {
+                _direction = value;
+                callCollapseDirectionChanged();
+            }
+        }
+
+
+        public event Action CollapseDirectionChanged;
+        protected void callCollapseDirectionChanged()
+        {
+            Action handler = CollapseDirectionChanged;
+            DisplayButtons();
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+        #endregion
+
+
+
+
+
+
+        private void DisplayButtons()
+        {
+            switch (_direction)
+            {
+                case CollapseDirection.Normal:
+                    break;
+
+                case CollapseDirection.Big:
+
+                    break;
+
+                case CollapseDirection.Wide:
+
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public iosButtons()
         {
             InitializeComponent();
