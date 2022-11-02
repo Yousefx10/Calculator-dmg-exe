26f727b2a4832bb296f5ef641c8a9ab2f6c89832
Yousef10 <yousef_espetan@yahoo.com>
2022-11-02 18:19:04 +0300

    Add Button Text

--- Calculator_dmg_exe/iosButtons.Designer.cs
+++ Calculator_dmg_exe/iosButtons.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTXT = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblTXT
+            // 
+            this.lblTXT.AutoSize = true;
+            this.lblTXT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTXT.Location = new System.Drawing.Point(18, 18);
+            this.lblTXT.Name = "lblTXT";
+            this.lblTXT.Size = new System.Drawing.Size(24, 25);
+            this.lblTXT.TabIndex = 0;
+            this.lblTXT.Text = "0";
             // 
             // iosButtons
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblTXT);
             this.Name = "iosButtons";
             this.Size = new System.Drawing.Size(60, 60);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTXT;
     }
 }
