2f5d5d7b65b923d20a4c1c9dd5ab14f19b05b5dc
Yousef10 <yousef_espetan@yahoo.com>
2022-11-02 18:08:17 +0300

    add smooth edges Border Radius

--- Calculator_dmg_exe/Form1.cs
+++ Calculator_dmg_exe/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,22 @@ namespace Calculator_dmg_exe
 {
     public partial class CalculatorForm : Form
     {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+(
+        int nLeftRect,     // x-coordinate of upper-left corner
+        int nTopRect,      // y-coordinate of upper-left corner
+        int nRightRect,    // x-coordinate of lower-right corner
+        int nBottomRect,   // y-coordinate of lower-right corner
+        int nWidthEllipse, // width of ellipse
+        int nHeightEllipse // height of ellipse
+);
+
         public CalculatorForm()
         {
             InitializeComponent();
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
         }
     }
 }
