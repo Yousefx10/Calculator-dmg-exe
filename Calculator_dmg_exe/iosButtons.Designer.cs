603dad02c6c03401abda4d9dde7d48e3b57db4e5
Yousef10 <yousef_espetan@yahoo.com>
2022-11-09 12:20:31 +0300

    new panel borders #3 to customize those borders easly

--- Calculator_dmg_exe/iosButtons.Designer.cs
+++ Calculator_dmg_exe/iosButtons.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.lblTXT = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // lblTXT
@@ -45,10 +49,46 @@
             this.lblTXT.Click += new System.EventHandler(this.Control_Click);
             this.lblTXT.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblTXT_MouseUp);
             // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 62);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(52, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 62);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(10, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(42, 10);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(10, 52);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(42, 10);
+            this.panel4.TabIndex = 4;
+            // 
             // iosButtons
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblTXT);
             this.Name = "iosButtons";
             this.Size = new System.Drawing.Size(62, 62);
@@ -60,5 +100,9 @@
         #endregion
 
         private System.Windows.Forms.Label lblTXT;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
